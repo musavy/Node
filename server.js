67d430d4797e38c5ee01e6af744eab00a802c198
sayed.musavy@integrify.io <sayed.musavy@integrify.io>
2019-05-28 15:35:53 +0300

    adding date and time and some files

--- server.js
+++ server.js
@@ -1,46 +1,78 @@
 //module
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
+const os = require("os");
+// const { displayDateTime } = require('./shared/display-day-time.js');
+// const { isEmpty } = require('./shared/is-empty.js');
+// console.log(displayDateTime());
+// console.log(isEmpty('exmaple@example.com'));
+const { displayDateTime, isEmpty, solveQuadratic } = require("./shared/utils");
+console.log(displayDateTime());
+console.log(isEmpty("Asab"));
+console.log(solveQuadratic(1, -1, -2));
 const PORT = 5000;
 
 const students = [
   {
-    name: 'Xurxe',
-    country: 'Galicia',
+    id: 1,
+    name: "Xurxe",
+    country: "Galicia",
     age: 25
   },
   {
-    name: 'Masood',
-    country: 'Afghan',
+    id: 2,
+    name: "Masood",
+    country: "Afghan",
     age: 25
   },
   {
-    name: 'Garland',
-    country: 'Indian',
+    id: 3,
+    name: "Garland",
+    country: "Indian",
     age: 21
   },
   {
-    name: 'Kunjan',
-    country: 'Nepal',
+    id: 4,
+    name: "Kunjan",
+    country: "Nepal",
     age: 21
   }
 ];
 
 http
-  .createServer(function (req, res) {
+  .createServer((req, res) => {
     const url = req.url;
     const method = req.method;
-    if (method === 'GET' && url === '/') {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write('<h1>Welcome Home<h1>');
-    } else if (method === 'GET' && url === '/info') {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.write(
-        'We are just starting Node.js. I hope everybody is enjoying it.'
-      );
-    } else if (method === 'GET' && url === '/students') {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.write(JSON.stringify(students));
-    }
-    res.end();
+
+    // fs.readFile('views/index.html', function(err, data) {
+    //   if (err) {
+    //     res.statusCode = 404;
+    //     console.log('error message', err.message);
+    //   }
+
+    //   res.writeHead(200, { 'Content-Type': 'text/html' });
+    //   res.write(data);
+    //  res.end();
+    // });
+    const hostname = os.hostname();
+    const platform = os.platform();
+    const content = hostname + " at " + displayDateTime() + " from " + platform+"\n";
+
+    fs.appendFile("log.txt", content, () => {
+      res.end();
+    });
+
+    // if (method === 'GET' && url === '/') {
+    //   res.write('Home');
+    // } else if (method === 'GET' && url === '/info') {
+    //   res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    //   res.write(
+    //     'We are just starting Node.js. I hope everybody is enjoying it.'
+    //   );
+    // } else if (method === 'GET' && url === '/students') {
+    //   res.writeHead(200, { 'Content-Type': 'application/json' });
+    //   res.write(JSON.stringify(students));
+    // }
   })
   .listen(PORT);
